1d4457eb54506bbe544ca88d6b3201775ca53602
chadrackmbala <chadrackmbala20@gmail.com>
2024-04-25 05:45:43 +0100

    fix clubs pages

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -3,7 +3,7 @@ import ActuDeux from "../components/actu2"
 export default function Events() {
     return(
         <>
-            <h2 className="text-[#319484] relative top-5 mb-10 text-center text-4xl">Evene;ents</h2>
+            <h2 className="text-[#319484] relative top-5 mb-10 text-center text-4xl">Evenements</h2>
             <div className="flex flex-wrap gap-20 justify-center">
                 {/* <Actus /> */}
                 <ActuDeux />
